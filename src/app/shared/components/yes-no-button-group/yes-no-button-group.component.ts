3bf7013dd24d6b63609c49c9d25bc85ab3a4deca
Edinardo Bezerra <edinardobezerra@yahoo.com.br>
2023-07-30 19:17:23 -0300

    Garantindo um ID único

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -2,7 +2,7 @@
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { YesNoButtonGroupModule } from './yes-no-button-group.module';
 import { Component, OnInit,Input, Output, EventEmitter, forwardRef } from '@angular/core';
-
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -20,11 +20,14 @@ export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
   @Input() public value: string = null;
   @Input() public label = "";
   @Output() public valueChange = new EventEmitter<string>();
+  public id: string = null;
   public options = YesNoButtonGroupOptions;
   public onChange = (value: string) =>{};
   public onTouched = () => {};
 
-  constructor() { }
+  constructor() {
+    this.id = `yes-no-button-group-${uuid.v1()}`;
+  }
 
   ngOnInit(): void {
   }
